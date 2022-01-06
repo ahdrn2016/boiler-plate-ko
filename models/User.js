f79b91ee4491a446d044940431c0e19e19684e74
ahdrn2016 <lyu9801@naver.com>
2022-01-07 01:14:56 +0900

    처음 저장소에 올림

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+// userSchema를 model로 감싸주기
+const User = mongoose.model('User', userSchema)
+
+// 'User'model을 다른 파일에서도 사용할 수 있게 exports
+module.exports = {User}
